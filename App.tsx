0b8ad9196e0a3a232559356bab2c4af0debda364
Lucas Johann <gabif.johann@gmail.com>
2024-09-18 09:34:23 -0300

    add: showing boarding pass

--- App.tsx
+++ App.tsx
@@ -1,5 +1,30 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+} from '@expo-google-fonts/poppins'
+import { StatusBar } from 'expo-status-bar'
+
 import { Home } from '@/app/home'
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Home />
+    </>
+  )
 }
--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,10 +1,79 @@
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import Svg, { Line, Circle } from 'react-native-svg'
+import QRCODE from 'react-native-qrcode-svg'
+
 import { styles } from './styles'
+import { colors } from '@/styles/color'
+import { Info } from '@/components/info'
+import { Flight } from '@/components/flight'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ImageBackground
+        style={styles.header}
+        source={require('@/assets/cover.jpg')}
+      >
+        <Text style={styles.title}>Cartão de embarque</Text>
+        <Text style={styles.subtitle}>Cartão de embarque</Text>
+      </ImageBackground>
+
+      <View style={styles.ticket}>
+        <View style={styles.content}>
+          <View style={styles.flight}>
+            <Flight label="São Paulo" value="GRU" />
+
+            <View style={styles.duration}>
+              <Ionicons name="airplane-sharp" size={32} color={colors.black} />
+              <Text style={styles.hours}>9h 45m</Text>
+            </View>
+            <Flight label="Nova York" value="JFK" />
+          </View>
+
+          <Text style={styles.label}>Passageiro</Text>
+          <Text style={styles.name}>Lucas Johann</Text>
+
+          <View style={styles.details}>
+            <View style={styles.inline}>
+              <Info label="Data" value="19 de Nov." />
+              <Info label="Embarque" value="17:25" />
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Svg height={20} width="100%">
+            <Line
+              x1="0%"
+              y1="50%"
+              x2="100%"
+              y2="50%"
+              stroke={colors.gray[400]}
+              strokeWidth={1}
+              strokeDasharray="5,5"
+            />
+            <Circle r={9} cx="0%" cy="50%" fill={colors.black} />
+            <Circle r={9} cx="100%" cy="50%" fill={colors.black} />
+          </Svg>
+        </View>
+
+        <View style={styles.footer}>
+          <View style={styles.footerContent}>
+            <View style={styles.inline}>
+              <Info label="Voo" value="YZ 607" />
+              <Info label="Acento" value="29G" />
+            </View>
+
+            <View style={styles.inline}>
+              <Info label="Terminal" value="3" />
+              <Info label="Portão" value="39" />
+            </View>
+          </View>
+
+          <QRCODE value="boarding code" size={130} />
+        </View>
+      </View>
     </View>
   )
 }
