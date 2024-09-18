ab1d30db43145c131a4355935ca2e20c4b4189cb
Lucas Johann <gabif.johann@gmail.com>
2024-09-18 09:33:33 -0300

    add: info component

---
+++ src/components/info/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+type InfoProps = {
+  label: string
+  value: string
+}
+
+export function Info({ label, value }: InfoProps) {
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  )
+}
